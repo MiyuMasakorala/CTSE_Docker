c01552fda36568ce1a3b30561e83866ac2612434
MiyuMasakorala <it19023410@my.sliit.lk>
2022-05-17 22:17:18 +0530

    functions productd

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -24,6 +24,47 @@ router.route("/add").post((req,res) =>{
     }).catch((err) => {
         console.log(err);
     })
+
+
+})
+
+router.route("/getAll").get((req,res)=>{
+    Product.find().then((products)=>{
+        res.json(products)
+    }).catch((error)=>{
+        console.log(error)
+    })
+})
+
+router.route("/update/:id").put(async(req,res) =>{
+    let userId = req.body.id;
+    const {name,price,description,stockAvailable,brandName,category} = req.body;
+
+    const updateProduct = {
+        name,
+        price,
+        description,
+        stockAvailable,
+        brandName,
+        category
+    }
+
+    const update = await Product.findByIdAndUpdate(userId,updateProduct)
+    .then(() => {
+        res.status(200).send({status: "Product Updated",user: update})
+    }).catch((error)=>{
+    res.status(500).send({status: "Error with update"})
+    })
+})
+
+router.route("/delete/:id").delete(async (req,res) => {
+    let userId = req.body.id;
+    await Product.findByIdAndDelete(userId)
+    .then(()=>{
+        res.status(200).send({status: "Product Deleted"})
+    }).catch((error)=>{
+        res.status(500).send({status: "Error with delete"})
+    })
 })
 
 module.exports = router;
